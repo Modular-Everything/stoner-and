bb608e1469600cb38a428d9a96de6f3b535e1ce7
Chrish Dunne <hey@chrish.design>
2021-03-24 11:46:34 +0000

    fix: Double <Layout /> bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Layout from '../components/Layout';
+
+//
 
 const HomePage = () => (
-  <Layout>
+  <>
     <p>Hello!</p>
-  </Layout>
+  </>
 );
 
 export default HomePage;
