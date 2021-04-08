f03c037d31e2f32bb747a40d42b9da48f36d0307
Chrish Dunne <hey@chrish.design>
2021-04-08 14:42:26 +0100

    style: Added additional paragraph style

--- src/components/Type/Copy.js
+++ src/components/Type/Copy.js
@@ -24,3 +24,26 @@ ParagraphSmall.propTypes = {
 ParagraphSmall.defaultProps = {
   as: null,
 };
+
+//
+
+export const ParagraphLarge = ({ children, as }) => (
+  <ParagraphLargeSt as={as}>{children}</ParagraphLargeSt>
+);
+
+const ParagraphLargeSt = styled.p`
+  font-family: var(--halyard-text);
+  font-size: 1.6rem;
+  font-weight: 300;
+  letter-spacing: 0.01rem;
+  line-height: 2.2rem;
+`;
+
+ParagraphLarge.propTypes = {
+  children: PropTypes.node.isRequired,
+  as: PropTypes.string,
+};
+
+ParagraphLarge.defaultProps = {
+  as: null,
+};
