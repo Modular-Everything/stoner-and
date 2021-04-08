ebd40d6dc3dc4825816ef9f1266bf8ff3ca9cf48
Chrish Dunne <hey@chrish.design>
2021-04-08 14:42:34 +0100

    feat: Convert headings to use clamp

--- src/components/Type/Headings.js
+++ src/components/Type/Headings.js
@@ -38,15 +38,10 @@ export const HeaderSerif = ({ children, as }) => (
 
 const HeaderSerifSt = styled.h1`
   font-family: var(--optima);
-  font-size: 4rem;
+  font-size: clamp(4rem, 4vw, 6.2rem);
   font-weight: normal;
   letter-spacing: -0.02rem;
-  line-height: 4rem;
-
-  @media (min-width: 640px) {
-    font-size: 5.6rem;
-    line-height: 6.2rem;
-  }
+  line-height: clamp(4rem, 3.2vw, 6.2rem);
 `;
 
 HeaderSerif.propTypes = {
@@ -55,5 +50,28 @@ HeaderSerif.propTypes = {
 };
 
 HeaderSerif.defaultProps = {
+  as: null,
+};
+
+//
+
+export const HeaderTwoSerif = ({ children, as }) => (
+  <HeaderTwoSerifSt as={as}>{children}</HeaderTwoSerifSt>
+);
+
+const HeaderTwoSerifSt = styled.h2`
+  font-family: var(--optima);
+  font-size: clamp(2.8rem, 3vw, 4.8rem);
+  font-weight: normal;
+  letter-spacing: 0.01rem;
+  line-height: clamp(3.4rem, 2.4vw, 3.4rem);
+`;
+
+HeaderTwoSerif.propTypes = {
+  children: PropTypes.node.isRequired,
+  as: PropTypes.string,
+};
+
+HeaderTwoSerif.defaultProps = {
   as: null,
 };
