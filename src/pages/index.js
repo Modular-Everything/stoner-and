b34b806c53ce3b7a3ea9967b4c8db3725790bb35
Chrish Dunne <hey@chrish.design>
2021-04-09 14:27:52 +0100

    feat: Pass props to introtext

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,15 @@ const HomePage = () => {
     <>
       <Content>
         <ContentContainer>
-          <IntroText />
+          <IntroText
+            title="Design that comes to life"
+            subtitle="Lorem ipsum"
+            copy={[
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla posuere sollicitudin aliquam ultrices sagittis. Lacus luctus accumsan tortor posuere ac. Sit amet venenatis urna cursus eget nunc. Sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus.',
+              'Orci nulla pellentesque dignissim enim sit amet venenatis urna. Risus quis varius quam quisque id diam vel quam. Commodo sed egestas egestas fringilla phasellus faucibus. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Eleifend quam.',
+            ]}
+          />
+
           <Signpost />
         </ContentContainer>
       </Content>
