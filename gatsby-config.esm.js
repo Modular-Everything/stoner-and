62127dd8d5a459261811e46b0e086934f1899de1
Chrish Dunne <hey@chrish.design>
2021-04-21 12:31:16 +0100

    feat: Added gatsby image support

--- gatsby-config.esm.js
+++ gatsby-config.esm.js
@@ -17,6 +17,9 @@ export default {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
