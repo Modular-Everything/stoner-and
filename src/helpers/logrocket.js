a62dc3567ba341c480ad61de88af368b0b88c377
Chrish Dunne <hey@chrish.design>
2021-05-04 09:51:06 +0100

    feat: Removed identity

--- src/helpers/logrocket.js
+++ src/helpers/logrocket.js
@@ -2,9 +2,4 @@ import LogRocket from 'logrocket';
 
 export default function logrocket() {
   LogRocket.init('rpltij/stoner');
-
-  LogRocket.identify('d14edf46-190f-4c23-a644-c5939e1f1f80', {
-    name: 'Chrish Dunne',
-    email: 'hey@chrish.design',
-  });
 }
