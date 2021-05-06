cd60570db085742484080ab6f07a6d5ee0cbcbd5
Chrish Dunne <hey@chrish.design>
2021-05-06 14:54:20 +0100

    fix: Missing property

--- src/helpers/logrocket.js
+++ src/helpers/logrocket.js
@@ -3,7 +3,7 @@ import LogRocket from 'logrocket';
 export default function logrocket() {
   const isProd = process.env.NODE_ENV === 'production';
   const isPreview =
-    typeof window !== 'undefined' && window.location.includes('preview');
+    typeof window !== 'undefined' && window.location.host.includes('preview');
 
   if (isProd || isPreview) {
     LogRocket.init('rpltij/stoner');
